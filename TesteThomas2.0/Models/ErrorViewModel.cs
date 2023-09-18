e8d268c0d51842eb363fec7e8efb0e932cec5323
luccaalison <luccaalison@icloud.com>
2023-09-17 23:49:47 -0300

    teste

--- TesteThomas2.0/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Projeto.Loja.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- TesteThomas2.0/Models/VendaProdutoDetailsModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Projeto.Loja.Models
-{
-    public class VendaProdutoDetailsModel
-    {
-        public string NomeProduto { get; set; }
-        public int ProdutoId { get; set; }
-        public decimal Preco { get; set; }
-    }
-}
